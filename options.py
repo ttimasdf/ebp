c6b4060c0b5514d289fae50b014db0ee0ce23f8e
ttimasdf <ttimasdf@users.noreply.github.com>
2017-04-24 17:52:33 +0800

    No more subclass Options

--- options.py
+++ options.py
@@ -1,20 +1,18 @@
-from .main import __doc__ as desc
-from argparse import ArgumentParser, Action
+from main import __doc__ as desc
+import util
+from argparse import ArgumentParser, FileType
 
 
-class Options(ArgumentParser):
-    "OptionParser specifically for Project"
+parser = ArgumentParser(description=desc)
 
-    def __init__(self):
-        super().__init__(description=desc)
-        self.add_argument("-c", "--conf",
-            dest="conf_file", help="Configuration file")
-        self.add_argument("-u", "--unsign",
-            action="store_true", default=False, help="Unsign executable if needed")
-        self.add_argument("-d", "--dry-run", "--verify",
-            action="store_true", default=False, help="Verify only")
-        self.add_argument("src",
-            required=True, help="Source file to patch")
-        self.add_argument("dst",
-            nargs="?", help="Destination file")
+parser.add_argument("-c", "--conf",
+    dest="conf_file", help="Configuration file")
+parser.add_argument("-u", "--unsign",
+    action="store_true", default=False, help="Unsign executable if needed")
+parser.add_argument("-d", "--dry-run", "--verify",
+    action="store_true", default=False, help="Verify only")
+parser.add_argument("source",
+    action=FileType("rb"), help="Source file to patch")
+parser.add_argument("dest",
+    action=FileType("r+b"), nargs="?", help="Destination file")
 
