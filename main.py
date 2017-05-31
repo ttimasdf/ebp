d0a58624a0f81c79cd0a05a83a3481d5267dbd24
ttimasdf <ttimasdf@users.noreply.github.com>
2017-05-31 08:39:37 +0800

    Stripped hardcoded package name
    For the preparation of pip distribution

--- main.py
+++ main.py
@@ -1,59 +1,6 @@
 #!/usr/bin/env python3
-"""
-EBP is a program which would patch (even large) binary
-efficiently based on binary fingerprints.
-"""
+from ebp.main import main
 
-from EBP import config, options, logging
-from EBP.patcher import Patcher
-from os.path import exists
-import shutil
-from unsign.unsign import unsign_macho
-
-def main():
-    args = options.get_args()
-    log = logging.get_logger(__package__, args.verbose)
-
-    if not exists(args.conf_file):
-        log.critical("Cannot find config file, exiting...")
-        raise SystemExit
-
-    conf = config.parse_file(args.conf_file)
-    m = conf['metadata']
-
-    print("""
-=========={len}====
-    EBP @ {name}
-=========={len}====
-
-{desc}
-""".format(
-    name=m['name'],
-    len='='*len(m['name']),
-    desc=m["description"],
-    ))
-
-    for name, info in conf['files'].items():
-        log.info("Patching {}...".format(name))
-
-        p = Patcher(info, basedir=args.source, test=args.test)
-        bak = shutil.copyfile(p.file, p.file.parent/(p.file.name+config.BACKUP_SUFFIX))
-        log.debug("Backed up to {}".format(bak))
-
-        if p.unsign:
-            unsign_macho(p.file.open('r+b'))
-            log.info("Unsign {} completed".format(name))
-
-        p.patch()
-
-    print("""
-Done!
-
-Advice from author:
-
-{congrat}
-""".format(congrat=m['congratulation']))
-    log.info("Patch completed successfully!")
 
 if __name__ == '__main__':
     main()
